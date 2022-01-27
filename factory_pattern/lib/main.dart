b6e91aaba4a6845c7ce40b2053b654effcaa16fe
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-01-27 10:13:53 +0300

    ref: clean project

--- factory_pattern/lib/main.dart
+++ factory_pattern/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:factory_pattern/pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +8,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
+
