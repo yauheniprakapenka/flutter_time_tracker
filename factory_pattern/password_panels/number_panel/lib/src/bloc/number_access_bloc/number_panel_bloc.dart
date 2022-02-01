bd80d0300d670d66da79a98fcabac290aa19c072
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-01 14:26:38 +0300

    ref: refactor number panel bloc

--- factory_pattern/password_panels/number_panel/lib/src/bloc/number_access_bloc/number_panel_bloc.dart
+++ factory_pattern/password_panels/number_panel/lib/src/bloc/number_access_bloc/number_panel_bloc.dart
@@ -8,42 +8,28 @@ import 'events/events.dart';
 import 'number_panel_state.dart';
 
 class NumberPanelBloc extends Bloc<INumberPanelEvent, NumberPanelState> {
-  String _enteredPassword = '';
+  static const _emptyPassword = '';
+  String _enteredPassword = _emptyPassword;
 
-  NumberPanelBloc() : super(const NumberPanelState(currentPassword: ''));
+  NumberPanelBloc() : super(const NumberPanelState(currentPassword: _emptyPassword));
 
   @override
   Stream<NumberPanelState> mapEventToState(INumberPanelEvent event) async* {
     if (event is PasswordButtonPressedEvent) {
       _enteredPassword += event.pressedValue;
       final enteredPasswordLength = _enteredPassword.length;
-      final _passwordLengthLimit =
-          UIServiceLocator.instance.get<IPasscodeConfig>().passcodeLength;
+      final _passwordLengthLimit = UIServiceLocator.instance.get<IPasscodeConfig>().passcodeLength;
       if (enteredPasswordLength >= _passwordLengthLimit) {
-        final _passcodeRepository =
-            DataServiceLocator.instance.get<IPasscodeRepository>();
-        final checkPasscodeHasMatchUseCase = CheckPasscodeHasMatchUseCase(
-            passcodeRepository: _passcodeRepository);
-        yield state.copyWith(
-            currentPassword: _enteredPassword); // Fill all indicators
-        await Future.delayed(
-          const Duration(milliseconds: 150),
-        ); // How many times show filled all indicators
-        final didPasswordMatch =
-            await checkPasscodeHasMatchUseCase(_enteredPassword);
+        final _passcodeRepository = DataServiceLocator.instance.get<IPasscodeRepository>();
+        final checkPasscodeHasMatchUseCase = CheckPasscodeHasMatchUseCase(passcodeRepository: _passcodeRepository);
+        yield state.copyWith(currentPassword: _enteredPassword); // Fill all indicators
+        await Future.delayed(const Duration(milliseconds: 150)); // How many times show filled all indicators
+        final didPasswordMatch = await checkPasscodeHasMatchUseCase(_enteredPassword);
         debugPrint('Did password has match: $didPasswordMatch');
-        if (didPasswordMatch) {
-          yield state.copyWith(
-            currentPassword: _enteredPassword,
-            passcodeResult: PasscodeResult.success,
-          );
-        } else {
-          yield state.copyWith(
-            currentPassword: _enteredPassword,
-            passcodeResult: PasscodeResult.fail,
-          );
-        }
-        _enteredPassword = '';
+        yield didPasswordMatch 
+          ? state.copyWith(currentPassword: _enteredPassword, passcodeResult: PasscodeResult.success)
+          : state.copyWith(currentPassword: _enteredPassword, passcodeResult: PasscodeResult.fail);
+        _clearEnteredPassword();
         return;
       }
       yield state.copyWith(currentPassword: _enteredPassword);
@@ -53,11 +39,14 @@ class NumberPanelBloc extends Bloc<INumberPanelEvent, NumberPanelState> {
     if (event is DeleteButtonPressedEvent) {
       final enteredPasswordLength = _enteredPassword.length;
       if (enteredPasswordLength == 0) return;
-      final newEnteredPassword =
-          _enteredPassword.substring(0, enteredPasswordLength - 1);
+      final newEnteredPassword = _enteredPassword.substring(0, enteredPasswordLength - 1);
       _enteredPassword = newEnteredPassword;
       yield state.copyWith(currentPassword: _enteredPassword);
       return;
     }
+  }
+
+  void _clearEnteredPassword() {
+    _enteredPassword = _emptyPassword;
   }
 }
