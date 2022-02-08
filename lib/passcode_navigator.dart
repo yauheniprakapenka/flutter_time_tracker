32781a343eb135b5e218b0e39dc73690a175c8cc
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-08 15:49:33 +0300

    fix: make public color and local

--- lib/passcode_navigator.dart
+++ lib/passcode_navigator.dart
@@ -2,3 +2,5 @@ library passcode_navigator;
 
 export 'src/app/facade/passcode_navigator.dart';
 export 'src/domain/entities/passcode_flow.dart';
+export 'src/ui/shared/localization/i_localization.dart';
+export 'src/ui/shared/theme/colors/i_color.dart';
