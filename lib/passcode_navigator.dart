b9959e5f55dee4b964b97cdc61213a0b63cd258b
Yauheni Prakapenka <yauheni.prakapenka@gmail.com>
2022-02-08 17:34:51 +0300

    fix: add public passcode result

--- lib/passcode_navigator.dart
+++ lib/passcode_navigator.dart
@@ -3,6 +3,6 @@ library passcode_navigator;
 export 'src/app/config/passcode_navigator_config.dart';
 export 'src/app/facade/passcode_navigator.dart';
 export 'src/domain/entities/passcode_flow.dart';
-export 'src/domain/entities/result.dart';
+export 'src/domain/entities/passcode_result.dart';
 export 'src/ui/shared/localization/i_localization.dart';
 export 'src/ui/shared/theme/colors/i_color.dart';
